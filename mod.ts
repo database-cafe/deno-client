8fc930bcf7bafc127a73b8fd1847e5023f2cf664
Michael Rybka <merybka@gmail.com>
2022-04-19 14:24:47 -0400

    Create first pass at a client

--- mod.ts
+++ mod.ts
@@ -1,3 +1,29 @@
-export function hello(): string {
-  return "hello world";
+export class Client {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  async listStructs(): Promise<Struct[]> {
+    const response = await fetch(`${this.endpoint}/structs`);
+    const data = await response.json();
+    if (data.Error !== "") {
+      throw new Error(data.Error);
+    }
+    return data.Data;
+  }
+}
+
+export interface Struct {
+  Name: string;
+  PluralName: string;
+  Fields: Field[];
+}
+
+export interface Field {
+  Name: string;
+  Description: string;
+  TypeID: string;
+  DefaultValue: string;
 }
